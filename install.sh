b0416afbbc12d969d1ab3a303eb6a697a1e7a63e
Erik Vroon <erik.vroon22@gmail.com>
2021-07-08 21:59:51 +0000

    Split install into setup and install

--- install.sh
+++ install.sh
@@ -1,39 +1,9 @@
-# Install dependencies
-sudo apt update && sudo apt upgrade
-sudo apt install -y \
-    wget curl python3 python3-pip certbot git make htop \
-    docker docker-compose uidmap \
-    munin munin-node libcgi-fast-perl libapache2-mod-fcgid \
-    postgresql-client pgpdump \
-    tmux update-notifier-common php7.4
-
-python3 -m pip3 install -r requirements.txt
-
-# ssh-keygen -t ed25519
-
-# Install docker rootless
-curl -fsSL https://get.docker.com/rootless | sh
-
-cat >> ~/.bashrc <<EOL
-# Docker
-export PATH=/home/azure/bin:$PATH
-export DOCKER_HOST=unix:///run/user/1000/docker.sock
-EOL
-
-
-# Prepare files for installation
-mkdir -p ${SELFOSS_DIR} ${MUNIN_DIR} ${DISCORD_DIR}
-TMP_DIR=$(mktemp -d)
-
-cd $TMP_DIR
-git clone https://github.com/evroon/concordia
-python3 build.py
-
 # Move files to correct locations
-cp etc/apache2/sites-available/* /etc/apache2/sites-available
-cp home/* home/.* ~/
-cp psql/* /var/lib/concordia
-cp selfoss/* ${CONCORDIA_DIR}
+mkdir -p ${SELFOSS_DIR} ${MUNIN_DIR} ${DISCORD_DIR}
+sudo cp etc/apache2/sites-available/* /etc/apache2/sites-available
+sudo cp home/.* ~/
+sudo cp psql/* /var/lib/concordia
+sudo cp selfoss/* ${CONCORDIA_DIR}
 
 # Change cron job
 sudo crontab crontab.sh
---
+++ setup.sh
@@ -0,0 +1,40 @@
+# Install dependencies
+#sudo apt update && sudo apt upgrade
+sudo apt install \
+    wget curl python3 python3-pip certbot git make htop \
+    docker docker-compose uidmap \
+    munin munin-node libcgi-fast-perl libapache2-mod-fcgid \
+    postgresql-client pgpdump \
+    tmux update-notifier-common php7.4
+
+python3 -m pip install -r requirements.txt
+
+# ssh-keygen -t ed25519
+
+# Install docker rootless
+#curl -fsSL https://get.docker.com/rootless | sh
+
+#cat >> ~/.bashrc <<EOL
+## Docker
+#export PATH=/home/azure/bin:$PATH
+#export DOCKER_HOST=unix:///run/user/1000/docker.sock
+#EOL
+
+
+# Prepare files for installation
+WORK_DIR=$(pwd)
+
+TMP_DIR=$(mktemp -d)
+echo "using temporary directory:" $TMP_DIR
+
+cd $TMP_DIR
+git clone -q https://github.com/evroon/concordia
+REPO_DIR=$TMP_DIR/concordia
+cd $REPO_DIR
+
+git checkout feature-installation
+
+cp $WORK_DIR/.env ./
+python3 build.py --directory=$REPO_DIR
+
+./install.sh
