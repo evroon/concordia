a50e6a227cec08e800c0dac1b1cb0d9d50cfefff
Erik Vroon <erik.vroon22@gmail.com>
2021-08-02 19:02:39 +0200

    bugfixes

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ sudo crontab crontab.sh
 # Install selfoss
 cd ${SELFOSS_DIR}
 sudo wget ${SELFOSS_RELEASE} -O selfoss.zip
-sudo unzip selfoss.zip
+sudo unzip -o selfoss.zip
 sudo rm selfoss.zip
 sudo chown www-data:www-data -R ${SELFOSS_DIR}
 
