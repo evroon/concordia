a4d5751745bd3ef1f411a52ce5b8e0f445474e32
Erik Vroon <erik.vroon22@gmail.com>
2021-08-02 22:01:34 +0200

    Use armv7 gitea

--- gitea/install.sh
+++ gitea/install.sh
@@ -1,11 +1,11 @@
-sudo wget -O /usr/bin/gitea https://dl.gitea.io/gitea/${GITEA_VERSION}/gitea-${GITEA_VERSION}-linux-amd64
+sudo wget -O /usr/bin/gitea https://dl.gitea.io/gitea/${GITEA_VERSION}/gitea-${GITEA_VERSION}-linux-arm-7
 sudo chown git:git /usr/bin/gitea
-sudo chmod 744 /usr/bin/gitea
+sudo chmod 755 /usr/bin/gitea
 
 gpg --keyserver keys.openpgp.org --recv 7C9E68152594688862D62AF62D9AE806EC1592E2
-gpg --verify gitea-${GITEA_VERSION}-linux-amd64.asc gitea-${GITEA_VERSION}-linux-amd64
+gpg --verify gitea-${GITEA_VERSION}-linux-arm-7.asc gitea-${GITEA_VERSION}-linux-arm-7
 
-adduser \
+sudo adduser \
    --system \
    --shell /bin/bash \
    --gecos 'Git Version Control' \
@@ -14,12 +14,12 @@ adduser \
    --home /home/git \
    git
 
-mkdir -p /var/lib/gitea/{custom,data,log}
-chown -R git:git /var/lib/gitea/
-chmod -R 750 /var/lib/gitea/
-mkdir /etc/gitea
-chown root:git /etc/gitea
-chmod 770 /etc/gitea
+sudo mkdir -p /var/lib/gitea/{custom,data,log}
+sudo chown -R git:git /var/lib/gitea/
+sudo chmod -R 750 /var/lib/gitea/
+sudo mkdir /etc/gitea
+sudo chown root:git /etc/gitea
+sudo chmod 770 /etc/gitea
 
 sudo systemctl enable gitea
 sudo systemctl start gitea
