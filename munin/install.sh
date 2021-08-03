f778778923ae874456a7284e386ee0a53a1d7355
Erik Vroon <erik.vroon22@gmail.com>
2021-08-03 12:36:47 +0200

    Add munin template

---
+++ munin/install.sh
@@ -0,0 +1,9 @@
+TMP_DIR=$(mktemp -d)
+cd $TMP_DIR
+git clone https://github.com/munin-monitoring/contrib.git
+
+cd /etc/munin
+sudo cp -r $TMP_DIR/contrib/templates/munstrap/templates .
+sudo cp -r $TMP_DIR/contrib/templates/munstrap/static .
+
+rm -rf /var/www/munin/*
