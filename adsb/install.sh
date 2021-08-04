a8d68833148c081afc2da96511ecf3356cf20833
Erik Vroon <erik.vroon22@gmail.com>
2021-08-04 11:22:29 +0200

    Bugfixes for piaware

---
+++ adsb/install.sh
@@ -0,0 +1,15 @@
+# See: https://discussions.flightaware.com/t/how-to-make-piaware-compatible-with-fr24feed/54045/6
+sudo bash -c "$(wget -O - https://repo-feed.flightradar24.com/install_fr24_rpi.sh)"
+sudo dpkg --purge dump1090-mutability
+
+# See: https://discussions.flightaware.com/t/piaware-v-3-7-1-on-debian-10-0-buster-amd64-intel-pc/52414/4
+sudo apt update
+sudo wget http://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_3.7.1_all.deb
+sudo dpkg -i piaware-repository_3.7.1_all.deb
+sudo apt-get update
+
+sudo apt-get install -y piaware
+sudo systemctl restart piaware
+
+sudo apt-get install -y dump1090-fa
+# sudo reboot
