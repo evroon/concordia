da984b015388965e452ff06f29cbe6f970d5c614
Erik Vroon <erik.vroon22@gmail.com>
2021-08-08 13:15:51 +0200

    Remove apache files

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
     wget curl python3 python3-pip git make htop tmux pwgen \
     docker docker-compose uidmap \
-    munin libcgi-fast-perl libapache2-mod-fcgid \
+    munin libcgi-fast-perl \
     postgresql postgresql-contrib postgresql-client pgpdump \
     php php-gd php-mbstring php-common php-pgsql php-imagick php-xml php-curl php-tidy php-zip \
-    certbot python3-certbot-nginx libcgi-fast-perl libapache2-mod-php libapache2-mod-fcgid \
+    certbot python3-certbot-nginx \
     mailutils msmtp msmtp-mta
 
 sudo pip3 install docker-compose python-dateutil
