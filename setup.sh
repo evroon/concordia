687efab566b2952a24a2523fcd96860f79d47c6c
Erik Vroon <erik.vroon22@gmail.com>
2021-10-14 21:32:13 +0200

    Don't install docker from apt repo's

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
     wget curl python3 python3-pip git make htop tmux pwgen \
-    docker docker-compose uidmap \
+    docker-compose uidmap \
     munin libcgi-fast-perl \
     postgresql postgresql-contrib postgresql-client pgpdump \
     php php-gd php-mbstring php-common php-pgsql php-imagick php-xml php-curl php-tidy php-zip \
