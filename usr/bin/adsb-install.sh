6432071e85d7081f9b76c04aca6a3f7a7b450b09
Erik Vroon <erik.vroon22@gmail.com>
2021-10-14 20:44:27 +0200

    Check for new version of PiAware

--- usr/bin/adsb-install.sh
+++ usr/bin/adsb-install.sh
@@ -2,13 +2,22 @@
 sudo bash -c "$(wget -O - https://repo-feed.flightradar24.com/install_fr24_rpi.sh)"
 sudo dpkg --purge dump1090-mutability
 
-# See: https://discussions.flightaware.com/t/piaware-v-3-7-1-on-debian-10-0-buster-amd64-intel-pc/52414/4
-sudo apt update
-sudo wget http://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_3.7.1_all.deb
-sudo dpkg -i piaware-repository_3.7.1_all.deb
-sudo apt-get update
+PIAWARE_VERSION=$(get_latest_github_release "flightaware/piaware")
+PIAWARE_VERSION="${PIAWARE_VERSION:1}"
+PIAWARE_VERSION_CURRENT=$(sed -n "s/^PIAWARE=\(.*\).*$/\1/p" versions.env)
 
-sudo apt-get install -y piaware
+if [ "$PIAWARE_VERSION" == "$PIAWARE_VERSION_CURRENT" ]; then
+    echo "PiAware is up-to-date (version: $PIAWARE_VERSION)"
+else
+    # See: https://discussions.flightaware.com/t/piaware-v-3-7-1-on-debian-10-0-buster-amd64-intel-pc/52414/4
+    sudo apt update
+    sudo wget https://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_$PIAWARE_VERSION_all.deb
+    sudo dpkg -i piaware-repository_$PIAWARE_VERSION_all.deb
+    sudo apt-get update
+
+    sudo apt-get install -y piaware    
+fi
+
 sudo systemctl restart piaware fr24feed
 
 sudo apt-get install -y dump1090-fa
